14cab2308690f70920d680b6ff94adc1c2f9c9cf
Logan Miner <loganrobertminer@gmail.com>
2019-10-15 10:11:31 -0400

    unpluralized controller filenames and users.js (api)

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,12 @@
 const router = require("express").Router();
 const usersController = require("../../controllers/usersController");
 
-router.route('/')
-    .get()
-router.route()
+router
+  .route("/")
+  .get(usersController.findAll)
+  .post(usersController.create);
+router
+  .route("/:id")
+  .get(usersController.findById)
+  .put(usersController.update)
+  .delete(usersController.remove);
