5a767f171fee76639de7cced0acdb7cd17868def
Chika <catherine.achukwu@gmail.com>
2019-10-17 16:06:02 -0400

    Budget form updated

--- client/src/pages/createBudget.js
+++ client/src/pages/createBudget.js
@@ -7,17 +7,17 @@ class createBudget extends React.Component {
     state = {
         grocery: "",
         dineOut: "",
-        alcohol: "", 
-        housing: "", 
-        apparerlAndServices: "", 
-        transportation:"", 
-        healthCare:"", 
-        entertainment:"", 
-        personalCare:"", 
-        education:"", 
-        miscallaneous:"", 
-        donations:"", 
-        insurance:""
+        alcohol: "",
+        housing: "",
+        apparerlAndServices: "",
+        transportation: "",
+        healthCare: "",
+        entertainment: "",
+        personalCare: "",
+        education: "",
+        miscallaneous: "",
+        donations: "",
+        insurance: ""
     };
 
     componentDidMount() { }
@@ -31,7 +31,21 @@ class createBudget extends React.Component {
 
     handleFormSubmit = event => {
         event.preventDefault();
-        this.setState({ username: "", password: "" });
+        this.setState({
+            grocery: "",
+            dineOut: "",
+            alcohol: "",
+            housing: "",
+            apparerlAndServices: "",
+            transportation: "",
+            healthCare: "",
+            entertainment: "",
+            personalCare: "",
+            education: "",
+            miscallaneous: "",
+            donations: "",
+            insurance: ""
+        });
     };
 
     render() {
@@ -41,55 +55,55 @@ class createBudget extends React.Component {
                 <form>
                     <div class="form-group">
                         Grocery
-                        <input type="grocery" class="form-control" name = "grocery" id="grocery" placeholder="Grocery" />
+                        <input type="grocery" class="form-control" name="grocery" id="grocery" placeholder="Grocery" />
                     </div>
                     <div class="form-group">
                         Dine Out
-                        <input type="dineout" class="form-control" name = "dineout" id="dineout" placeholder="Dineout" />
+                        <input type="dineout" class="form-control" name="dineout" id="dineout" placeholder="Dineout" />
                     </div>
                     <div class="form-group">
                         Alcohol
-                        <input type="Alcohol" class="form-control" name = "Alcohol" id="Alcohol" placeholder="Alcohol" />
+                        <input type="Alcohol" class="form-control" name="Alcohol" id="Alcohol" placeholder="Alcohol" />
                     </div>
                     <div class="form-group">
                         Housing
-                        <input type="Housing" class="form-control" name = "Housing" id="Housing" placeholder="Housing" />
+                        <input type="Housing" class="form-control" name="Housing" id="Housing" placeholder="Housing" />
                     </div>
                     <div class="form-group">
                         Apparel and Services
-                        <input type="Apparel" class="form-control" name = "Apparel" id="Apparel" placeholder="Apparel" />
+                        <input type="Apparel" class="form-control" name="Apparel" id="Apparel" placeholder="Apparel" />
                     </div>
                     <div class="form-group">
                         Transportation
-                        <input type="Transportation" class="form-control" name = "Transportation" id="Transportation" placeholder="Transportation" />
+                        <input type="Transportation" class="form-control" name="Transportation" id="Transportation" placeholder="Transportation" />
                     </div>
                     <div class="form-group">
                         Health Care
-                        <input type="password" class="form-control" name = "HealthCare" id="HealthCare" placeholder="HealthCare" />
+                        <input type="password" class="form-control" name="HealthCare" id="HealthCare" placeholder="HealthCare" />
                     </div>
                     <div class="form-group">
                         Entertainment
-                        <input type="password" class="form-control" name = "Entertainment" id="Entertainment" placeholder="Entertainment" />
+                        <input type="password" class="form-control" name="Entertainment" id="Entertainment" placeholder="Entertainment" />
                     </div>
                     <div class="form-group">
                         Personal Care
-                        <input type="password" class="form-control" name = "PersonalCare" id="PersonalCare" placeholder="PersonalCare" />
+                        <input type="password" class="form-control" name="PersonalCare" id="PersonalCare" placeholder="PersonalCare" />
                     </div>
                     <div class="form-group">
                         Education
-                        <input type="password" class="form-control" name = "Education" id="Education" placeholder="Education" />
+                        <input type="password" class="form-control" name="Education" id="Education" placeholder="Education" />
                     </div>
                     <div class="form-group">
                         Miscallaneous
-                        <input type="password" class="form-control" name = "Miscallaneous" id="Miscallaneous" placeholder="Miscallaneous" />
+                        <input type="password" class="form-control" name="Miscallaneous" id="Miscallaneous" placeholder="Miscallaneous" />
                     </div>
                     <div class="form-group">
                         Donations
-                        <input type="password" class="form-control" name = "Donations" id="Donations" placeholder="Donations" />
+                        <input type="password" class="form-control" name="Donations" id="Donations" placeholder="Donations" />
                     </div>
                     <div class="form-group">
                         Insurance
-                        <input type="password" class="form-control" name = "Insurance" id="Insurance" placeholder="Insurance" />
+                        <input type="password" class="form-control" name="Insurance" id="Insurance" placeholder="Insurance" />
                     </div>
                     <button type="submit" class="btn btn-primary">Submit</button>
                 </form>
