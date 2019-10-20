79e2e6cf8231230640bf503cfcc224505d635be7
Logan Miner <loganrobertminer@gmail.com>
2019-10-20 09:42:25 -0400

    jwt

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,8 +1,10 @@
 const router = require("express").Router();
 const userRoutes = require("./users");
 const budgetRoutes = require("./budgets");
+const loginRoutes = require('./login')
 
 router.use("/users", userRoutes);
 router.use("/budgets", budgetRoutes);
+router.use('/login', loginRoutes)
 
 module.exports = router;
--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 const routes = require("./routes");
 const mongoose = require("mongoose");
+const jwt = require("jsonwebtoken");
 
 // Middleware
 app.use(express.json());
@@ -20,6 +21,20 @@ app.use(routes);
 
 // Connection to database
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/reactgreen");
+
+
+
+// app.post("/api/login", (req, res) => {
+//   // mock user
+//   const user = {
+//     id: 1,
+//     username: "logan",
+//     email: "logan@gmail.com"
+//   };
+//   jwt.sign({ user }, "secretkey", (err, token) => {
+//     res.json({ token });
+//   });
+// });
 
 // App listening
 app.listen(PORT, () => {
